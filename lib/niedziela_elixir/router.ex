bde991f66f076d7926e6f2cf485ef7b51c4ce03d
Anna Anks Nowak <matisnape@wp.pl>
2019-08-14 15:15:32 +0200

    Add router

---
+++ lib/niedziela_elixir/router.ex
@@ -0,0 +1,35 @@
+defmodule NiedzielaElixir.Router do
+  use Plug.Router
+  use Plug.ErrorHandler
+
+  alias Example.Plug.VerifyRequest
+
+  plug Plug.Parsers, parsers: [:urlencoded, :multipart]
+  plug VerifyRequest, fields: ["content", "mimetype"], paths: ["/upload"]
+
+  plug :match
+  plug :dispatch
+
+  get "/" do
+    send_resp(conn, 200, "Welcome")
+  end
+
+  get "/hello" do
+    send_resp(conn, 200, "Hello world")
+  end
+
+  get "/upload" do
+    send_resp(conn, 201, "Uploaded")
+  end
+
+  match _ do
+    send_resp(conn, 404, "Oops!")
+  end
+
+  defp handle_errors(conn, %{kind: kind, reason: reason, stack: stack}) do
+    IO.inspect(kind, label: :kind)
+    IO.inspect(reason, label: :reason)
+    IO.inspect(stack, label: :stack)
+    send_resp(conn, conn.status, "Something went wrong")
+  end
+end
