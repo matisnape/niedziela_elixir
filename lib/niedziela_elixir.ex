c33d0284a3547a1b0ec166bd3467875ba3759aca
Anna Anks Nowak <matisnape@wp.pl>
2019-08-14 12:57:22 +0200

    Initial commit

---
+++ lib/niedziela_elixir.ex
@@ -0,0 +1,18 @@
+defmodule NiedzielaElixir do
+  @moduledoc """
+  Documentation for NiedzielaElixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NiedzielaElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/niedziela_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule NiedzielaElixirTest do
+  use ExUnit.Case
+  doctest NiedzielaElixir
+
+  test "greets the world" do
+    assert NiedzielaElixir.hello() == :world
+  end
+end
