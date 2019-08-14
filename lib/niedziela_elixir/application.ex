00ea6c433030bc1caf00d0ef6857bdd90afc6314
Anna Anks Nowak <matisnape@wp.pl>
2019-08-14 13:42:51 +0200

    Hello world

--- lib/niedziela_elixir/application.ex
+++ lib/niedziela_elixir/application.ex
@@ -4,16 +4,21 @@ defmodule NiedzielaElixir.Application do
   @moduledoc false
 
   use Application
+  require Logger
 
   def start(_type, _args) do
     children = [
       # Starts a worker by calling: NiedzielaElixir.Worker.start_link(arg)
       # {NiedzielaElixir.Worker, arg}
+      {Plug.Cowboy, scheme: :http, plug: NiedzielaElixir.HelloWorldPlug, options: [port: 8080]}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: NiedzielaElixir.Supervisor]
+
+    Logger.info("Starting application...")
+
     Supervisor.start_link(children, opts)
   end
 end
